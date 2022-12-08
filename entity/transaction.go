d3f5cccf9e73a5b1cef6d3c2df3187d971eea8d2
bernardokowacic <berkowacic@gmail.com>
2022-12-07 21:23:37 -0300

    feat: create transaction entity

---
+++ entity/transaction.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type Transaction struct {
+	ID              uint64        `json:"id" gorm:"primaryKey;column:id"`
+	AccountID       uint64        `json:"account_id" binding:"required" gorm:"type:uint;column:account_id;<-:create;not null"`
+	OperationTypeID uint64        `json:"operation_type_id" binding:"required" gorm:"type:uint;column:operation_type_id;<-:create;not null"`
+	Account         Account       `json:"-"`
+	OperationType   OperationType `json:""`
+	Amount          float64       `json:"amount" binding:"required" gorm:"type:decimal(10,2);column:amount;not null"`
+	CreatedAt       time.Time     `json:"created_at" gorm:"autoCreateTime;not null"`
+}
