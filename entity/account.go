0ae3ce47734f8edf6097ca405294afedbfa1aea9
bernardokowacic <berkowacic@gmail.com>
2022-12-08 20:36:25 -0300

    refactor: insert uri name for account id field

--- entity/account.go
+++ entity/account.go
@@ -1,6 +1,6 @@
 package entity
 
 type Account struct {
-	ID             uint64 `json:"id" gorm:"primaryKey;column:id"`
+	ID             uint64 `json:"id" uri:"accountId" gorm:"primaryKey;column:id"`
 	DocumentNumber uint64 `json:"document_number" gorm:"unique;type:uint;column:document_number;<-:create;not null"`
 }
