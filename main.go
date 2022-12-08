4b3bc0916f787aa992256955204461ae858e181e
bernardokowacic <berkowacic@gmail.com>
2022-12-07 21:25:30 -0300

    feat: create env reader and logger initialization

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"pismo/api"
+	"pismo/database"
+	"pismo/repository"
+	"pismo/service/account"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	zerolog.SetGlobalLevel(zerologLogLevel(os.Getenv("INFO_LEVEL")))
+}
+
+func main() {
+	dbConn, err := database.CreatePGConn()
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+
+	database.Migrate(dbConn)
+	database.Seed(dbConn)
+
+	accountRepository := repository.NewRepository(dbConn)
+	accountService := account.NewService(accountRepository)
+
+	router := api.Start(accountService)
+	log.Info().Msg("API Started")
+	router.Run()
+}
+
+func zerologLogLevel(level string) zerolog.Level {
+	switch level {
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "error":
+		return zerolog.ErrorLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "fatal":
+		return zerolog.FatalLevel
+	case "panic":
+		return zerolog.PanicLevel
+	default:
+		panic(fmt.Sprintf("the specified %s log level is not supported", level))
+	}
+}
