c329bde5c61ad7f076e7b584060a726258f64939
bernardokowacic <berkowacic@gmail.com>
2022-12-07 21:48:08 -0300

    refactor: change account document number to unique

--- entity/account.go
+++ entity/account.go
@@ -2,5 +2,5 @@ package entity
 
 type Account struct {
 	ID             uint64 `json:"id" gorm:"primaryKey;column:id"`
-	DocumentNumber uint64 `json:"user_id" gorm:"type:uint;column:document_number;<-:create;not null"`
+	DocumentNumber uint64 `json:"document_number" gorm:"unique;type:uint;column:document_number;<-:create;not null"`
 }
