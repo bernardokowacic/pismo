f79a86b0aaa8cc6f6b8dc17845c4c7410b69fc3d
bernardokowacic <berkowacic@gmail.com>
2022-12-07 21:22:41 -0300

    feat: create account controller

---
+++ controller/account.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"net/http"
+	"pismo/entity"
+	"pismo/service/account"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func InsertAccount(AccountService account.AccountServiceInterface) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		log.Debug().Msg("end-point POST /accounts requested")
+
+		var postData entity.Account
+		err := ctx.ShouldBindJSON(&postData)
+		if err != nil {
+			log.Warn().Msg(err.Error())
+			ctx.JSON(http.StatusNotAcceptable, gin.H{"message": err.Error()})
+			return
+		}
+
+		_, err = AccountService.Insert(postData)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		log.Debug().Msg("end-point GET /accounts finished")
+
+		ctx.JSON(http.StatusOK, gin.H{})
+	}
+}
