a7b6eb0d03cd8a7ff9fe9336e3bc043f1efcffc3
bernardokowacic <berkowacic@gmail.com>
2022-12-07 21:47:19 -0300

    refactor: insert database host into env file

--- database/pgconn.go
+++ database/pgconn.go
@@ -9,7 +9,7 @@ import (
 )
 
 func CreatePGConn() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=database user=postgres password=%s dbname=pismo port=5432 sslmode=disable TimeZone=America/Sao_Paulo", os.Getenv("POSTGRES_PASSWORD"))
+	dsn := fmt.Sprintf("host=%s user=postgres password=%s dbname=pismo port=5432 sslmode=disable TimeZone=America/Sao_Paulo", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PASSWORD"))
 	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
 	if err != nil {
 		return nil, err
