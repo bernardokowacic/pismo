3857e59fcf25c116de3bb0ab761f542c34e351bd
bernardokowacic <berkowacic@gmail.com>
2022-12-08 21:39:40 -0300

    feat: add Fk to transaction

--- database/migrations.go
+++ database/migrations.go
@@ -10,4 +10,6 @@ func Migrate(db *gorm.DB) {
 	db.Migrator().CreateTable(&entity.Account{})
 	db.Migrator().CreateTable(&entity.OperationType{})
 	db.Migrator().CreateTable(&entity.Transaction{})
+	db.Migrator().CreateConstraint(&entity.Transaction{}, "fk_operation_types_transactions")
+	db.Migrator().CreateConstraint(&entity.Transaction{}, "fk_accounts_transactions")
 }
