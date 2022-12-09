2e16b6fd1ffc6d0e91b953bad509916d5497539a
bernardokowacic <berkowacic@gmail.com>
2022-12-08 21:40:51 -0300

    feat: add updated_at field

--- entity/transaction.go
+++ entity/transaction.go
@@ -1,13 +1,14 @@
 package entity
 
-import "time"
+import (
+	"time"
+)
 
 type Transaction struct {
-	ID              uint64        `json:"id" gorm:"primaryKey;column:id"`
-	AccountID       uint64        `json:"account_id" binding:"required" gorm:"type:uint;column:account_id;<-:create;not null"`
-	OperationTypeID uint64        `json:"operation_type_id" binding:"required" gorm:"type:uint;column:operation_type_id;<-:create;not null"`
-	Account         Account       `json:"-"`
-	OperationType   OperationType `json:""`
-	Amount          float64       `json:"amount" binding:"required" gorm:"type:decimal(10,2);column:amount;not null"`
-	CreatedAt       time.Time     `json:"created_at" gorm:"autoCreateTime;not null"`
+	ID              uint64    `json:"id" gorm:"primaryKey;column:id"`
+	AccountID       uint64    `json:"account_id" binding:"required" gorm:"type:uint;column:account_id;<-:create;not null"`
+	OperationTypeID uint64    `json:"operation_type_id" binding:"required" gorm:"type:uint;column:operation_type_id;<-:create;not null"`
+	Amount          float64   `json:"amount" binding:"required" gorm:"type:decimal(10,2);column:amount;not null"`
+	CreatedAt       time.Time `json:"created_at" gorm:"autoCreateTime;not null"`
+	UpdatedAt       time.Time `json:"-" gorm:"autoUpdateTime;not null"`
 }
