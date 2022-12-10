9c24dd64a931c04c300bd44030b165232c6e6391
bernardokowacic <berkowacic@gmail.com>
2022-12-09 21:49:11 -0300

    refactor: change document number type

--- entity/account.go
+++ entity/account.go
@@ -2,5 +2,5 @@ package entity
 
 type Account struct {
 	ID             uint64 `json:"id" uri:"accountId" gorm:"primaryKey;column:id"`
-	DocumentNumber uint64 `json:"document_number" gorm:"unique;type:uint;column:document_number;<-:create;not null"`
+	DocumentNumber string `json:"document_number" gorm:"unique;type:string;size:300;column:document_number;<-:create;not null"`
 }
