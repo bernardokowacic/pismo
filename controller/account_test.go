b4ff265b39df1dde32ad8fa8cf08b4b1bcef5971
bernardokowacic <berkowacic@gmail.com>
2022-12-09 21:49:49 -0300

    feat: add unit tests for account insert

---
+++ controller/account_test.go
@@ -0,0 +1,89 @@
+package controller_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"pismo/api"
+	"pismo/entity"
+	serviceMock "pismo/mocks/service"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestInsertAccount(t *testing.T) {
+	accountService := &serviceMock.AccountServiceInterface{}
+	transactionService := &serviceMock.TransactionServiceInterface{}
+	router := api.Start(accountService, transactionService)
+
+	args := struct {
+		DocumentNumber string `json:"document_number"`
+	}{
+		DocumentNumber: "12345678900",
+	}
+	body, _ := json.Marshal(args)
+
+	request := entity.Account{
+		DocumentNumber: args.DocumentNumber,
+	}
+
+	response := entity.Account{
+		ID:             1,
+		DocumentNumber: args.DocumentNumber,
+	}
+
+	accountService.On("Insert", request).Return(response, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/accounts", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestInsertAccountWithBindingError(t *testing.T) {
+	accountService := &serviceMock.AccountServiceInterface{}
+	transactionService := &serviceMock.TransactionServiceInterface{}
+	router := api.Start(accountService, transactionService)
+
+	args := struct {
+		DocumentNumber uint `json:"document_number"`
+	}{
+		DocumentNumber: 12345678900,
+	}
+	body, _ := json.Marshal(args)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/accounts", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotAcceptable, w.Code)
+}
+
+func TestInsertAccountWithServiceError(t *testing.T) {
+	accountService := &serviceMock.AccountServiceInterface{}
+	transactionService := &serviceMock.TransactionServiceInterface{}
+	router := api.Start(accountService, transactionService)
+
+	args := struct {
+		DocumentNumber string `json:"document_number"`
+	}{
+		DocumentNumber: "12345678900",
+	}
+	body, _ := json.Marshal(args)
+
+	request := entity.Account{
+		DocumentNumber: args.DocumentNumber,
+	}
+
+	accountService.On("Insert", request).Return(entity.Account{}, errors.New("error"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/accounts", bytes.NewReader(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
