72cb3f9a34d59998dee08b4c30e8e0a0269a1e03
bernardokowacic <berkowacic@gmail.com>
2022-12-14 15:31:25 -0300

    refactor: change transaction service to update the account balance in each transaction

--- service/transaction.go
+++ service/transaction.go
@@ -33,7 +33,7 @@ func NewTransactionService(
 func (t *transactiontService) Insert(transaction entity.Transaction) (entity.Transaction, error) {
 	log.Debug().Msg("Creating new transaction")
 
-	_, err := t.accountRepository.Find(transaction.AccountID)
+	account, err := t.accountRepository.Find(transaction.AccountID)
 	if err != nil {
 		log.Warn().Msg("account not found")
 		return entity.Transaction{}, errors.New("account not found")
@@ -43,6 +43,18 @@ func (t *transactiontService) Insert(transaction entity.Transaction) (entity.Tra
 	if err != nil {
 		log.Warn().Msg("operation type not found")
 		return entity.Transaction{}, errors.New("operation type not found")
+	}
+
+	newBalance := account.AvailableCreditLimit + transaction.Amount
+	if newBalance < 0 {
+		log.Debug().Msg("not enough credit in the account")
+		return entity.Transaction{}, errors.New("not enough credit in the account")
+	}
+
+	err = t.accountRepository.UpdateBalance(account.ID, newBalance)
+	if err != nil {
+		log.Error().Msg(err.Error())
+		return entity.Transaction{}, err
 	}
 
 	response, err := t.TransactionRepository.Insert(transaction)
