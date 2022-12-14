c427d125c85d539648131f8a3d957c418aadcfd3
bernardokowacic <berkowacic@gmail.com>
2022-12-14 15:29:55 -0300

    feat: add new parameter to account struct

--- entity/account.go
+++ entity/account.go
@@ -1,6 +1,7 @@
 package entity
 
 type Account struct {
-	ID             uint64 `json:"id" uri:"accountId" gorm:"primaryKey;column:id"`
-	DocumentNumber string `json:"document_number" gorm:"unique;type:string;size:300;column:document_number;<-:create;not null"`
+	ID                   uint64  `json:"id" uri:"accountId" gorm:"primaryKey;column:id"`
+	DocumentNumber       string  `json:"document_number" gorm:"unique;type:string;size:300;column:document_number;<-:create;not null"`
+	AvailableCreditLimit float64 `json:"available_credit_limit" binding:"required" gorm:"type:decimal(10,2);column:available_creadit_limit;not null"`
 }
